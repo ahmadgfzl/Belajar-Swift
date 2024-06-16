cca7819eddb323a95da6d8aa863bb24a0c855fb4
ahmadgfzl <ahmadgibranfzl@gmail.com>
2024-06-17 05:47:26 +0700

    Belahjar-Swift

---
+++ Belajar-Swift/Belajar_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Belajar_SwiftApp.swift
+//  Belajar-Swift
+//
+//  Created by Ahmad Gibran Faizal on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Belajar_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Belajar-Swift/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Belajar-Swift
+//
+//  Created by Ahmad Gibran Faizal on 14/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
